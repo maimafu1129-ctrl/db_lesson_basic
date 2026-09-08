e84551225d762a4d08c66b406d1523b68e4c9bee
maimafu1129-ctrl <mai.mafu.1129@gmail.com>
2026-09-08 12:07:28 +0900

    sql課作成

--- db_lesson.sql
+++ db_lesson.sql
@@ -1,111 +1,203 @@
--- MySQL dump 10.13  Distrib 8.0.46, for macos14.8 (x86_64)
---
--- Host: localhost    Database: db_lesson
--- ------------------------------------------------------
--- Server version	8.0.46
+-- Q1 部署テーブル追加
+create table `department`(
+   department_id INT unsigned not null auto_increment PRIMARY KEY,
+   name VARCHAR(20) not null,
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   updated_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+   );
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+show tables;
++---------------------+
+| Tables_in_db_lesson |
++---------------------+
+| department          |
+| people              |
+| reports             |
++---------------------+
 
---
--- Table structure for table `department`
---
+-- Q2 peopleテーブルに部署カラムを追加
+ ALTER TABLE people ADD department_id INT UNSIGNED AFTER email;
+ DESC people;
++---------------+------------------+------+-----+-------------------+-----------------------------------------------+
+| Field         | Type             | Null | Key | Default           | Extra                                         |
++---------------+------------------+------+-----+-------------------+-----------------------------------------------+
+| person_id     | int unsigned     | NO   | PRI | NULL              | auto_increment                                |
+| name          | varchar(20)      | NO   |     | NULL              |                                               |
+| email         | varchar(255)     | YES  | UNI | NULL              |                                               |
+| department_id | int unsigned     | YES  |     | NULL              |                                               |
+| age           | tinyint unsigned | YES  |     | NULL              |                                               |
+| gender        | tinyint          | YES  |     | NULL              |                                               |
+| created_at    | timestamp        | YES  |     | CURRENT_TIMESTAMP | DEFAULT_GENERATED                             |
+| updated_at    | timestamp        | YES  |     | CURRENT_TIMESTAMP | DEFAULT_GENERATED on update CURRENT_TIMESTAMP |
++---------------+------------------+------+-----+-------------------+-----------------------------------------------+
 
-DROP TABLE IF EXISTS `department`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `department` (
-  `department_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`department_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Q3-1 departmentsテーブルに営業、開発、経理、人事、情報システムのレコードを追加
+INSERT INTO department (name)
+    -> values
+    -> ('営業'),
+    -> ('開発'),
+    -> ('経理'),
+    -> ('人事'),
+    -> ('情報システム');
 
---
--- Dumping data for table `department`
---
+SELECT *FROM  department;
++---------------+--------------------+---------------------+---------------------+
+| department_id | name               | created_at          | updated_at          |
++---------------+--------------------+---------------------+---------------------+
+|             1 | 営業               | 2026-09-07 18:45:10 | 2026-09-07 18:45:10 |
+|             2 | 開発               | 2026-09-07 18:45:10 | 2026-09-07 18:45:10 |
+|             3 | 経理               | 2026-09-07 18:45:10 | 2026-09-07 18:45:10 |
+|             4 | 人事               | 2026-09-07 18:45:10 | 2026-09-07 18:45:10 |
+|             5 | 情報システム       | 2026-09-07 18:45:10 | 2026-09-07 18:45:10 |
++---------------+--------------------+---------------------+---------------------+
 
-LOCK TABLES `department` WRITE;
-/*!40000 ALTER TABLE `department` DISABLE KEYS */;
-INSERT INTO `department` VALUES (1,'営業','2026-09-07 09:45:10','2026-09-07 09:45:10'),(2,'開発','2026-09-07 09:45:10','2026-09-07 09:45:10'),(3,'経理','2026-09-07 09:45:10','2026-09-07 09:45:10'),(4,'人事','2026-09-07 09:45:10','2026-09-07 09:45:10'),(5,'情報システム','2026-09-07 09:45:10','2026-09-07 09:45:10');
-/*!40000 ALTER TABLE `department` ENABLE KEYS */;
-UNLOCK TABLES;
+-- Q3-2 10人分のレコードを追加する,人数比率は営業3人、開発4人、経理1人、人事1人、情報システム1人
+-- 存在する部署のIDが割り振られること/どう割り振るかは指定しませんが、必ずWHEREを使って条件を絞ってください
+UPDATE people SET department_id = 1 WHERE person_id IN (1);
+UPDATE people SET department_id = 1 WHERE person_id IN (2);
+UPDATE people SET department_id = 1 WHERE person_id IN (12);
+UPDATE people SET department_id = 2 WHERE person_id IN (4);
+UPDATE people SET department_id = 2 WHERE person_id IN (7);
+UPDATE people SET department_id = 2 WHERE person_id IN (9);
+UPDATE people SET department_id = 3 WHERE person_id IN (6);
+UPDATE people SET department_id = 4 WHERE person_id IN (3);
+UPDATE people SET department_id = 5 WHERE person_id IN (8);
+UPDATE people SET department_id = 2 WHERE person_id IN (10);
 
---
--- Table structure for table `people`
---
+SELECT * FROM people;
++-----------+-----------------------+-----------------------+---------------+------+--------+---------------------+---------------------+
+| person_id | name                  | email                 | department_id | age  | gender | created_at          | updated_at          |
++-----------+-----------------------+-----------------------+---------------+------+--------+---------------------+---------------------+
+|         1 | 鈴木たかし            | suzuki@gizumo.jp      |             1 |   20 |      1 | 2026-09-04 17:54:29 | 2026-09-07 22:17:06 |
+|         2 | 田中ゆうこ            | tanaka@gizumo.jp      |             1 |   25 |      2 | 2026-09-04 17:54:29 | 2026-09-07 22:21:33 |
+|         3 | 福田だいすけ          | fukuda@gizumo.jp      |             4 |   42 |      1 | 2026-09-04 17:54:29 | 2026-09-07 22:23:02 |
+|         4 | 豊島はなこ            | toyoshima@gizumo.jp   |             2 |   34 |      2 | 2026-09-04 17:54:29 | 2026-09-07 22:09:28 |
+|         6 | 不思議沢みちこ        | hushigi@gizumo.jp     |             3 |   35 |   NULL | 2026-09-04 17:54:29 | 2026-09-07 22:22:48 |
+|         7 | 山田花子              | yamada@gizumo.jp      |             2 |   28 |      2 | 2026-09-05 23:22:50 | 2026-09-07 22:22:26 |
+|         8 | 佐藤健一              | sato@gizumo.jp        |             5 |   35 |      1 | 2026-09-05 23:22:50 | 2026-09-07 22:25:47 |
+|         9 | 高橋美咲              | takahashi@gizumo.jp   |             2 |   24 |      2 | 2026-09-05 23:22:50 | 2026-09-07 22:22:32 |
+|        10 | 伊藤直樹              | ito@gizumo.jp         |             2 |   42 |      1 | 2026-09-05 23:22:50 | 2026-09-07 22:26:04 |
+|        12 | 早坂てつお            | hayasaka@gizumo.co.jp |             1 |   61 |      1 | 2026-09-06 20:22:34 | 2026-09-07 22:21:41 |
++-----------+-----------------------+-----------------------+---------------+------+--------+---------------------+---------------------+
 
-DROP TABLE IF EXISTS `people`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `people` (
-  `person_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `department_id` int unsigned DEFAULT NULL,
-  `age` tinyint unsigned DEFAULT NULL,
-  `gender` tinyint DEFAULT NULL COMMENT '1が男、2が女',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`person_id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Q3-3 10件の日報を追加する
+ INSERT INTO reports (person_id, content)
+    -> VALUES
+    ->  (1, '本日は午前中に商品データの確認を行いました。午後は入力作業を進めました。'),
+    -> (2, ' 本日はメールの確認と資料の整理を行いました。予定していた作業を完了しました'),
+    -> (3, '本日はデータの更新作業を行いました。入力内容に間違いがないか確認しました。'),
+    -> (4, '本日はチームで作業内容の確認を行いました。その後、担当している作業を進めました。'),
+    -> (6, '本日はデータベースの確認を行いました。いくつかのデータを修正しました。'),
+    -> (7, '本日は午前中に資料を作成し、午後は内容の確認と修正を行いました。'),
+    -> (8,'本日は新しい作業の手順を確認しました。実際に作業を行い、基本的な流れを理解しました。'),
+    -> (9, '本日は登録されている情報を確認しました。不要なデータを整理して作業環境を整えました。'),
+    -> (10, '本日は担当している作業を予定通り進めました。最後に今日の作業内容を確認しました。');
 
---
--- Dumping data for table `people`
---
+    SELECT p.name,r.content FROM people p LEFT OUTER JOIN reports r USING (person_id);
++-----------------------+-----------------------------------------------------------------------------------------------------------------------------------------------------+
+| name                  | content                                                                                                                                             |
++-----------------------+-----------------------------------------------------------------------------------------------------------------------------------------------------+
+| 鈴木たかし            | 本日は午前中に商品データの確認を行いました。午後は入力作業を進めました。                                                                            |
+| 田中ゆうこ            | 本日はメールの確認と資料の整理を行いました。予定していた作業を完了しました                                                                          |
+| 福田だいすけ          | 本日はデータの更新作業を行いました。入力内容に間違いがないか確認しました。                                                                          |
+| 豊島はなこ            | 本日はチームで作業内容の確認を行いました。その後、担当している作業を進めました。                                                                    |
+| 不思議沢みちこ        | 本日はデータベースの確認を行いました。いくつかのデータを修正しました。                                                                              |
+| 山田花子              | 本日は午前中に資料を作成し、午後は内容の確認と修正を行いました。                                                                                    |
+| 佐藤健一              | 本日は新しい作業の手順を確認しました。実際に作業を行い、基本的な流れを理解しました。                                                                |
+| 高橋美咲              | 本日は登録されている情報を確認しました。不要なデータを整理して作業環境を整えました。                                                                |
+| 伊藤直樹              | 本日は担当している作業を予定通り進めました。最後に今日の作業内容を確認しました。                                                                    |
+| 渡辺愛                | 本日はシステムの動作確認を行いました。登録されているデータを確認し、問題がないことを確認しました。                                                  |
++-----------------------+-----------------------------------------------------------------------------------------------------------------------------------------------------+
 
-LOCK TABLES `people` WRITE;
-/*!40000 ALTER TABLE `people` DISABLE KEYS */;
-INSERT INTO `people` VALUES (1,'鈴木たかし','suzuki@gizumo.jp',1,20,1,'2026-09-04 08:54:29','2026-09-07 13:17:06'),(2,'田中ゆうこ','tanaka@gizumo.jp',1,25,2,'2026-09-04 08:54:29','2026-09-07 13:21:33'),(3,'福田だいすけ','fukuda@gizumo.jp',4,42,1,'2026-09-04 08:54:29','2026-09-07 13:23:02'),(4,'豊島はなこ','toyoshima@gizumo.jp',2,34,2,'2026-09-04 08:54:29','2026-09-07 13:09:28'),(6,'不思議沢みちこ','hushigi@gizumo.jp',3,35,NULL,'2026-09-04 08:54:29','2026-09-07 13:22:48'),(7,'山田花子','yamada@gizumo.jp',2,28,2,'2026-09-05 14:22:50','2026-09-07 13:22:26'),(8,'佐藤健一','sato@gizumo.jp',5,35,1,'2026-09-05 14:22:50','2026-09-07 13:25:47'),(9,'高橋美咲','takahashi@gizumo.jp',2,24,2,'2026-09-05 14:22:50','2026-09-07 13:22:32'),(10,'伊藤直樹','ito@gizumo.jp',2,42,1,'2026-09-05 14:22:50','2026-09-07 13:26:04'),(12,'早坂てつお','hayasaka@gizumo.co.jp',1,61,1,'2026-09-06 11:22:34','2026-09-07 13:21:41');
-/*!40000 ALTER TABLE `people` ENABLE KEYS */;
-UNLOCK TABLES;
+-- Q4　department_id NULLを確認
+SELECT * FROM people WHERE department_id IS NULL;
 
---
--- Table structure for table `reports`
---
+-- Q5　年齢の降順で男性の名前と年齢を取得してください。
+SELECT name ,age  from people where gender = 1  ORDER BY age DESC;
++--------------------+------+
+| name               | age  |
++--------------------+------+
+| 早坂てつお         |   61 |
+| 福田だいすけ       |   42 |
+| 伊藤直樹           |   42 |
+| 佐藤健一           |   35 |
+| 鈴木たかし         |   20 |
++--------------------+------+
 
-DROP TABLE IF EXISTS `reports`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `reports` (
-  `report_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `person_id` int unsigned NOT NULL,
-  `content` text NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`report_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Q6　テーブル・レコード・カラムという3つの単語を適切に使用して、下記のSQL文を日本語で説明
+-- 問題文
+SELECT
+  `name`, `email`, `age`
+FROM
+  `people`
+WHERE
+  `department_id` = 1
+ORDER BY
+  `created_at`;
+-- 回答
+-- peopleテーブルにあるdepartment_idが1（営業）の方の’name’’email’’age’のデータをcreated_atの時間が古い順に表示してください。
 
---
--- Dumping data for table `reports`
---
+-- Q7 20代の女性と40代の男性の名前一覧を取得
+SELECT name FROM people WHERE gender = 2 AND age >= 20 and age < 30 OR gender = 1 AND age >= 40 AND age < 50;
++--------------------+
+| name               |
++--------------------+
+| 田中ゆうこ         |
+| 福田だいすけ       |
+| 山田花子           |
+| 高橋美咲           |
+| 伊藤直樹           |
++--------------------+
 
-LOCK TABLES `reports` WRITE;
-/*!40000 ALTER TABLE `reports` DISABLE KEYS */;
-INSERT INTO `reports` VALUES (1,1,'本日は午前中に商品データの確認を行いました。午後は入力作業を進めました。','2026-09-06 07:32:31','2026-09-06 07:32:31'),(2,2,'本日はメールの確認と資料の整理を行いました。予定していた作業を完了しました','2026-09-06 07:32:31','2026-09-06 07:32:31'),(3,3,'本日はデータの更新作業を行いました。入力内容に間違いがないか確認しました。','2026-09-06 07:32:31','2026-09-06 07:32:31'),(4,4,'本日はチームで作業内容の確認を行いました。その後、担当している作業を進めました。','2026-09-06 07:32:31','2026-09-06 07:32:31'),(5,6,'本日はデータベースの確認を行いました。いくつかのデータを修正しました。','2026-09-06 07:32:31','2026-09-06 07:32:31'),(6,7,'本日は午前中に資料を作成し、午後は内容の確認と修正を行いました。','2026-09-06 07:32:31','2026-09-06 07:32:31'),(7,8,'本日は新しい作業の手順を確認しました。実際に作業を行い、基本的な流れを理解しました。','2026-09-06 07:32:31','2026-09-06 07:32:31'),(8,9,'本日は登録されている情報を確認しました。不要なデータを整理して作業環境を整えました。','2026-09-06 07:32:31','2026-09-06 07:32:31'),(9,10,'本日は担当している作業を予定通り進めました。最後に今日の作業内容を確認しました。','2026-09-06 07:32:31','2026-09-06 07:32:31'),(10,11,'本日はシステムの動作確認を行いました。登録されているデータを確認し、問題がないことを確認しました。','2026-09-06 07:42:10','2026-09-06 07:42:10');
-/*!40000 ALTER TABLE `reports` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- Q8 営業部に所属する人だけを年齢の昇順で取得
+ SELECT age FROM people WHERE department_id = 1 ORDER BY age ASC;
+ +------+
+| age  |
++------+
+|   20 |
+|   25 |
+|   61 |
++------+
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- Q9 開発部に所属している女性の平均年齢を取得
+SELECT AVG(age) as average_age FROM people WHERE gender = 2 AND department_id = 2;
++-------------+
+| average_age |
++-------------+
+|     28.6667 |
++-------------+
 
--- Dump completed on 2026-09-07 23:22:59
+-- Q10 名前と部署名とその人が提出した日報の内容を同時に取得 NULLは含めない
+SELECT people. name, department. name, reports.content FROM people INNER JOIN department ON people.department_id = department.department_id INNER JOIN reports USING(person_id);
++-----------------------+--------------------+--------------------------------------------------------------------------------------------------------------------------------+
+| name                  | name               | content                                                                                                                        |
++-----------------------+--------------------+--------------------------------------------------------------------------------------------------------------------------------+
+| 鈴木たかし            | 営業               | 本日は午前中に商品データの確認を行いました。午後は入力作業を進めました。                                                       |
+| 田中ゆうこ            | 営業               | 本日はメールの確認と資料の整理を行いました。予定していた作業を完了しました                                                     |
+| 福田だいすけ          | 人事               | 本日はデータの更新作業を行いました。入力内容に間違いがないか確認しました。                                                     |
+| 豊島はなこ            | 開発               | 本日はチームで作業内容の確認を行いました。その後、担当している作業を進めました。                                               |
+| 不思議沢みちこ        | 経理               | 本日はデータベースの確認を行いました。いくつかのデータを修正しました。                                                         |
+| 山田花子              | 開発               | 本日は午前中に資料を作成し、午後は内容の確認と修正を行いました。                                                               |
+| 佐藤健一              | 情報システム       | 本日は新しい作業の手順を確認しました。実際に作業を行い、基本的な流れを理解しました。                                           |
+| 高橋美咲              | 開発               | 本日は登録されている情報を確認しました。不要なデータを整理して作業環境を整えました。                                           |
+| 伊藤直樹              | 開発               | 本日は担当している作業を予定通り進めました。最後に今日の作業内容を確認しました。                                               |
++-----------------------+--------------------+--------------------------------------------------------------------------------------------------------------------------------+
+
+-- Q11　日報を一つも提出していない人の名前一覧を取得
+SELECT p.name,r.content FROM people p LEFT OUTER JOIN reports r USING (person_id);
++-----------------------+--------------------------------------------------------------------------------------------------------------------------------+
+| name                  | content                                                                                                                        |
++-----------------------+--------------------------------------------------------------------------------------------------------------------------------+
+| 鈴木たかし            | 本日は午前中に商品データの確認を行いました。午後は入力作業を進めました。                                                       |
+| 田中ゆうこ            | 本日はメールの確認と資料の整理を行いました。予定していた作業を完了しました                                                     |
+| 福田だいすけ          | 本日はデータの更新作業を行いました。入力内容に間違いがないか確認しました。                                                     |
+| 豊島はなこ            | 本日はチームで作業内容の確認を行いました。その後、担当している作業を進めました。                                               |
+| 不思議沢みちこ        | 本日はデータベースの確認を行いました。いくつかのデータを修正しました。                                                         |
+| 山田花子              | 本日は午前中に資料を作成し、午後は内容の確認と修正を行いました。                                                               |
+| 佐藤健一              | 本日は新しい作業の手順を確認しました。実際に作業を行い、基本的な流れを理解しました。                                           |
+| 高橋美咲              | 本日は登録されている情報を確認しました。不要なデータを整理して作業環境を整えました。                                           |
+| 伊藤直樹              | 本日は担当している作業を予定通り進めました。最後に今日の作業内容を確認しました。                                               |
+| 早坂てつお            | NULL                                                                                                                           |
++-----------------------+--------------------------------------------------------------------------------------------------------------------------------+
+
+
